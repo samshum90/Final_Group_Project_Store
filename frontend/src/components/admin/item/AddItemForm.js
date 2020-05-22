808e49b56ae54fed4db82f72793e05c61250fab9
Sam <samhum90@gmail.com>
2020-05-22 13:53:12 +0100

    added

---
+++ frontend/src/components/admin/item/AddItemForm.js
@@ -0,0 +1,112 @@
+import React, {Component} from 'react';
+
+class AddItemForm extends Component {
+  constructor(props){
+    super(props);
+  this.state = {  
+    name: '',
+    type: '',
+    salePrice: 0,
+    buyPrice: 0,
+    img: '',
+    description: ''
+  }
+
+}
+
+handleNameChange = (event) => {
+  this.setState({name: event.target.value})
+}
+
+handleTypeChange = (event) => {
+  this.setState({type: event.target.value})
+}
+
+handleSalePriceChange = (event) => {
+  this.setState({salePrice: event.target.value})
+}
+
+handleBuyPriceChange = (event) => {
+  this.setState({buyPrice: event.target.value})
+}
+
+handleImgChange = (event) => {
+  this.setState({img: event.target.value})
+}
+
+handleDescriptionChange = (event) => {
+  this.setState({description: event.target.value})
+}
+
+
+
+
+  render() { 
+    return ( 
+    <div>
+      <form>
+      <label htmlFor="name">Name: </label>
+      <input 
+        type="text" 
+        id="name"
+        value={this.state.name}
+        onChange={this.handleNameChange}
+      ></input> 
+
+      <label htmlFor="brand">Brand: </label>
+      <input 
+      type="text" 
+      id="brand"
+      value={this.state.brand}
+      onChange={this.handleBrandChange}
+      ></input> 
+
+      <label htmlFor="type">Type: will be dropdown  </label>
+      <input 
+      type="text" 
+      id="type"
+      value={this.state.type}
+      onChange={this.handleTypeChange}
+      ></input>
+
+      <label htmlFor="sale-price">List Price: </label>
+      <input 
+      type="number" 
+      id="sale-price"
+      value={this.state.salePrice}
+      onChange={this.handleSalePriceChange}
+      ></input>
+
+      <label htmlFor="buy-price">Unit Cost: </label>
+      <input 
+      type="number" 
+      id="buy-price"
+      value={this.state.buyPrice}
+      onChange={this.handleBuyPriceChange}
+      ></input>  
+
+      <label htmlFor="image-url">URL to Image: </label>
+      <input 
+      type="text" 
+      id="image-url"
+      value={this.state.img}
+      onChange={this.handleImgChange}
+      ></input> 
+
+      <label htmlFor="description">Description: </label>
+      <input 
+      type="text" 
+      id="description"
+      value={this.state.description}
+      onChange={this.handleDescriptionChange}
+      ></input> 
+      
+      <input type="submit" value="Add item" />
+
+      </form>
+      </div> 
+      );
+  }
+}
+ 
+export default AddItemForm;
--- frontend/src/containers/AdminContainer.js
+++ frontend/src/containers/AdminContainer.js
@@ -1,9 +1,60 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import AddItemForm from '../components/admin/item/AddItemForm'
 
 class AdminContainer extends Component {
-  state = {  }
+  constructor(props){
+    super(props);
+  this.state = {  
+    orders: []
+    }
+  }
+  handlePost(item){
+    const request = new Request();
+    request.post('/items', item).then(() => {
+      window.location = '/items'
+    })
+  }
   render() { 
-    return ( <h1>I can do things users cannot</h1> );
+    const routes = [
+      {
+        path: "/addItem",
+        exact: true,
+        main: () => <AddItemForm onFormSubmit= {this.handlePost}/>
+      },
+    ];
+    return ( 
+      <Router>
+      <div>
+        <nav className="admin-nav-bar">
+          <ul>
+            <li>
+              <Link to="/items">Show Items</Link>
+            </li>
+            <li>
+              <Link to="/addItem">Add Item</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+            {routes.map((route, index) => (
+                                <Route
+                                key={index}
+                                path={route.path}
+                                exact={route.exact}
+                                children={<route.main />}
+                                />
+            ))}
+        </Switch>
+      </div>
+    </Router>
+     );
   }
 }
  
