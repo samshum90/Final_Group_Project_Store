06871b554ea4f14e79d4a1126a0a16054b73efe0
Sam <samhum90@gmail.com>
2020-05-22 16:57:44 +0100

    added

--- frontend/src/components/admin/item/ItemList.js
+++ frontend/src/components/admin/item/ItemList.js
@@ -12,10 +12,9 @@ const ItemList = (props) => {
 	const items = props.items.map((item, index) => {
         
 	  return (
-	    <li key={index}>
-	    <div>
+	    <li key={item.id}>
 	    <ItemDetail item={item} />
-	    </div>
+	
 	    </li>
       )
       
