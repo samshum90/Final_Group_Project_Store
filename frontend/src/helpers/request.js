70600e02a42ee76179e0f52d74aae20a362cd710
Sam <samhum90@gmail.com>
2020-05-25 22:51:37 +0100

    stashing

--- frontend/src/helpers/request.js
+++ frontend/src/helpers/request.js
@@ -18,7 +18,8 @@ class Request {
 	post(url, payload) {
 		return fetch(url, {
 			method: 'POST',
-			headers: { 'Content-Type': 'application/json' },
+			headers: { 
+				'Content-Type': 'application/json' },
 			body: JSON.stringify(payload),
 		}).catch((err) => console.log(err));
 	}
