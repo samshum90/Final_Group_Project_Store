d1a990f9acd5784e2e6d4a37a50da5e877ae8a47
michaeld55 <michaeld55@hotmail.co.uk>
2020-05-25 18:05:10 +0100

    mergered develop into test_new_user

--- frontend/src/helpers/request.js
+++ frontend/src/helpers/request.js
@@ -16,7 +16,9 @@ class Request {
 	}
 
 	post(url, payload) {
+		console.log(payload);
 		return fetch(url, {
+			
 			method: 'POST',
 			headers: { 'Content-Type': 'application/json' },
 			body: JSON.stringify(payload),
